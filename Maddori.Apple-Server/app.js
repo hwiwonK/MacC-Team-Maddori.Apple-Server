8aaa9855618a6e3c238dbd759c80e91000edf72e
hwiwonK <gnldnjs2469@gmail.com>
2022-10-22 18:45:45 +0900

    [SETTING] server-database 연결(#2)

--- Maddori.Apple-Server/app.js
+++ Maddori.Apple-Server/app.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 
+// sequelize 연결
+const { sequelize } = require('./models');
+
+// database 연결
+//db 연결
+
+sequelize.sync({ force: false })
+.then(() => {
+    console.log('데이터베이스 연결 성공');
+})
+.catch((err) => {
+    console.log('데이터베이스 연결 실패');
+    console.error(err);
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
