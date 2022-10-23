7ca6b7fed4f2c285264305929ef347e1aab98094
hwiwonK <gnldnjs2469@gmail.com>
2022-10-23 20:18:28 +0900

    [FEAT] team 테이블 추가(#7)

---
+++ Maddori.Apple-Server/models/team.js
@@ -0,0 +1,42 @@
+'use strict';
+const { Model } = require("sequelize");
+// user 테이블 만들어서 User 객체로 저장하기
+
+module.exports = function(sequelize, DataTypes){
+    class team extends Model {
+        
+        static associate(models) {
+
+        }
+    }
+
+    team.init(
+        {
+            id: {
+                field: "id",
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            team_name: {
+                field: "team_name",
+                type: DataTypes.STRING(10),
+                allowNull: false
+            },
+            invitation_code: {
+                field: "invitation_code",
+                type: DataTypes.STRING(6),
+                allowNull: false
+            }
+        }, {
+            sequelize,
+            modelName: "team",
+            timestamps: false,
+            freezeTableName: true,
+            charset: "utf8",
+            collate: "utf8_general_ci",
+            underscored: true
+        }
+    );
+    return team;
+}
