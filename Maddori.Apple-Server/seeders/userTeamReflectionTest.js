675f209a6abec331548f717e712a7cd1769d162f
hwiwonK <gnldnjs2469@gmail.com>
2022-11-01 12:01:59 +0900

    [FEAT] feedback seed data 추가(#10)

--- Maddori.Apple-Server/seeders/userTeamReflectionTest.js
+++ Maddori.Apple-Server/seeders/userTeamReflectionTest.js
@@ -112,6 +112,136 @@ module.exports = {
         team_name: 'Academy🍎'
       }
     });
+
+    // feedback data
+    const feedbacks = [
+      {
+        from_id: userId1,
+        to_id: userId1 + 1,
+        team_id: teamId1,
+        reflection_id: reflectionId1,
+        type: "Continue",
+        keyword: "c",
+        content: "지속하기"
+      },
+      {
+        from_id: userId1,
+        to_id: userId1 + 1,
+        team_id: teamId1,
+        reflection_id: reflectionId1,
+        type: "Stop",
+        keyword: "s",
+        content: "그만하기"
+      },
+      {
+        from_id: userId1,
+        to_id: userId1 + 2,
+        team_id: teamId1,
+        reflection_id: reflectionId1,
+        type: "Continue",
+        keyword: "c",
+        content: "지속하기"
+      },
+      {
+        from_id: userId1 + 1,
+        to_id: userId1 + 2,
+        team_id: teamId1,
+        reflection_id: reflectionId1,
+        type: "Stop",
+        keyword: "s",
+        content: "멈추기",
+        start_content: "시작하기"
+      },
+      {
+        from_id: userId1 + 1,
+        to_id: userId1,
+        team_id: teamId1,
+        reflection_id: reflectionId1,
+        type: "Continue",
+        keyword: "c",
+        content: "지속하기",
+        start_content: "시작하기"
+      },
+      {
+        from_id: userId1,
+        to_id: userId1 + 1,
+        team_id: teamId1,
+        reflection_id: reflectionId1 + 1,
+        type: "Continue",
+        keyword: "c",
+        content: "지속하기"
+      },
+      {
+        from_id: userId1,
+        to_id: userId1 + 1,
+        team_id: teamId1,
+        reflection_id: reflectionId1 + 1,
+        type: "Stop",
+        keyword: "s",
+        content: "그만하기"
+      },
+      {
+        from_id: userId1,
+        to_id: userId1 + 2,
+        team_id: teamId1,
+        reflection_id: reflectionId1 + 1,
+        type: "Continue",
+        keyword: "c",
+        content: "지속하기"
+      },
+      {
+        from_id: userId1 + 1,
+        to_id: userId1 + 2,
+        team_id: teamId1,
+        reflection_id: reflectionId1 + 1,
+        type: "Stop",
+        keyword: "s",
+        content: "멈추기",
+        start_content: "시작하기"
+      },
+      {
+        from_id: userId1 + 1,
+        to_id: userId1,
+        team_id: teamId1,
+        reflection_id: reflectionId1 + 1,
+        type: "Continue",
+        keyword: "c",
+        content: "지속하기",
+        start_content: "시작하기"
+      },
+      {
+        from_id: userId1 + 4,
+        to_id: userId1 + 3,
+        team_id: teamId1 + 1,
+        reflection_id: reflectionId1 + 2,
+        type: "Continue",
+        keyword: "c",
+        content: "지속하기"
+      },
+      {
+        from_id: userId1 + 4,
+        to_id: userId1 + 3,
+        team_id: teamId1 + 1,
+        reflection_id: reflectionId1 + 2,
+        type: "Stop",
+        keyword: "s",
+        content: "그만하기",
+        start_content: "시작하기"
+      },      
+      {
+        from_id: userId1 + 3,
+        to_id: userId1 + 4,
+        team_id: teamId1 + 1,
+        reflection_id: reflectionId1 + 2,
+        type: "Continue",
+        keyword: "c",
+        content: "지속하기",
+        start_content: "시작하기"
+      }      
+    ]
+
+    // feedback insert data
+    await queryInterface.bulkInsert('feedback', feedbacks);
   },
 
   async down (queryInterface, Sequelize) {
