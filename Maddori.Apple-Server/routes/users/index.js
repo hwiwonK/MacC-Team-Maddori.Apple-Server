f5109226096a5e87e3d5f4ce6191b3c6838ef99c
hwiwonK <gnldnjs2469@gmail.com>
2022-11-02 03:18:53 +0900

    [FEAT] setup 페이지-join team api 구현(#13)

--- Maddori.Apple-Server/routes/users/index.js
+++ Maddori.Apple-Server/routes/users/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = new express.Router({ mergeParams: true });
 const {
     userLogin,
+    userJoinTeam
 } = require('./users');
 
 router.post('/login', userLogin);
+router.post('/join-team', userJoinTeam);
 
 module.exports = router;
--- Maddori.Apple-Server/routes/users/users.js
+++ Maddori.Apple-Server/routes/users/users.js
@@ -20,6 +20,42 @@ async function userLogin(req, res, next) {
     }
 }
 
+async function userJoinTeam(req, res, next) {
+    console.log("유저 팀 조인");
+    const userTeamContent = req.body;
+    console.log(userTeamContent);
+    // TODO: 데이터 형식 맞지 않는 경우 에러 처리 추가
+
+    try {
+        const requestTeam = await team.findOne({
+            where : {
+                invitation_code: userTeamContent.invitation_code
+            },
+            raw : true
+        });
+        // 초대 코드가 일치하는 팀이 없을 경우
+        if (requestTeam == null) {
+            console.log("team not found");
+            // TODO: 초대 코드가 잘못 됐을 경우 에러 처리 추가
+            // res.status(400).send({err_msg: "wrong invitation code"});
+        }
+        // 초대 코드가 일치하는 팀이 있는 경우, userteam 테이블 업데이트
+        console.log(requestTeam);
+        const createdUserTeam = await userteam.create({
+            user_id: req.header('user_id'),
+            team_id: requestTeam.id
+        });
+        console.log(createdUserTeam);
+        res.status(201).send(createdUserTeam);
+        // TODO: response 과정 에러 처리 추가
+
+    } catch(error) {
+        // TODO: 에러 처리 수정
+        res.status(500).send(error);
+    }
+}
+
 module.exports = {
     userLogin,
+    userJoinTeam
 };
