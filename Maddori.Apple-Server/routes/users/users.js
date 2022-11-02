a923021a352172e840dcc71f870d2382e07a9281
hwiwonK <gnldnjs2469@gmail.com>
2022-11-02 17:27:25 +0900

    [FEAT] setup 페이지-create team api 구현(#13)

--- Maddori.Apple-Server/routes/users/users.js
+++ Maddori.Apple-Server/routes/users/users.js
@@ -1,7 +1,9 @@
 const {user, team, userteam, reflection, feedback} = require('../../models');
 
 // TODO : social login, token 생성 방식으로 변경
-// user의 name을 받아 새로운 user 생성하기
+// request data : username
+// response data : user_id, username
+// 새로운 user 생성하기
 async function userLogin(req, res, next) {
     console.log("유저 로그인");
     console.log(req.body);
@@ -20,6 +22,9 @@ async function userLogin(req, res, next) {
     }
 }
 
+// request data : user_id, invitation_code
+// response data : userteam_id, user_id, team_id
+// 유저가 팀에 합류하기
 async function userJoinTeam(req, res, next) {
     console.log("유저 팀 조인");
     const userTeamContent = req.body;
@@ -41,20 +46,22 @@ async function userJoinTeam(req, res, next) {
         }
         // 초대 코드가 일치하는 팀이 있는 경우, userteam 테이블 업데이트
         console.log(requestTeam);
-        const createdUserTeam = await userteam.create({
+        const createdUserteam = await userteam.create({
             user_id: req.header('user_id'),
             team_id: requestTeam.id
         });
-        console.log(createdUserTeam);
-        res.status(201).send(createdUserTeam);
+        console.log(createdUserteam);
+        res.status(201).send(createdUserteam);
         // TODO: response 과정 에러 처리 추가
-
     } catch(error) {
         // TODO: 에러 처리 수정
         res.status(500).send(error);
     }
 }
 
+// request data : user_id, team_id
+// response data : 결과 처리 여부
+// 유저가 팀을 탈퇴하기
 async function userLeaveTeam(req, res, next) {
     console.log("유저 팀 탈퇴");
     
@@ -66,13 +73,13 @@ async function userLeaveTeam(req, res, next) {
             }
         });
         // TODO: 삭제가 제대로 수행 안됐을 경우 에러 처리 추가
+        // TODO: 삭제할 데이터가 없을 경우 에러 처리 수정
         console.log(deletedUserTeam);
         if (deletedUserTeam == 1) {
             res.status(200).send("success");
         } else {
             res.status(202).send("no such data");
         }
-
     } catch(error) {
         res.status(500).send(error);
     }
