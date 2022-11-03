2994bbc04e0a33f40f65430fe35ae51b76915d45
hwiwonK <gnldnjs2469@gmail.com>
2022-11-03 17:27:08 +0900

    [FEAT]  home view - get 현재 reflection api 구현(#17)

--- Maddori.Apple-Server/routes/reflections/index.js
+++ Maddori.Apple-Server/routes/reflections/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = new express.Router({ mergeParams: true });
+const {
+    getRelfectionInformation
+} = require('./reflections');
 
+router.get('/current', getRelfectionInformation);
 
 module.exports = router;
