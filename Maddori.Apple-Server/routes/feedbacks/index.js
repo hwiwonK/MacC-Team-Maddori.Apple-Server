5fe254528e50285c616ecda3c8159cbf1aac8f30
hwiwonK <gnldnjs2469@gmail.com>
2022-11-03 23:58:07 +0900

    [FEAT] addFeedback view - feedback 등록하기 api 구현(#21)

--- Maddori.Apple-Server/routes/feedbacks/index.js
+++ Maddori.Apple-Server/routes/feedbacks/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = new express.Router({ mergeParams: true });
+const {
+    createFeedback
+} = require('./feedbacks');
 
+router.post('/', createFeedback);
 
 module.exports = router;
