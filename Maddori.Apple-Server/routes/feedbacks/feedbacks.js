264e52ba22dcb23497f9c8c36694d3ebd211095d
unanchoi <unusualc@likelion.org>
2022-11-06 23:40:55 +0900

    [FEAT] 최근 회고 피드백 조회 API 추가

--- Maddori.Apple-Server/routes/feedbacks/feedbacks.js
+++ Maddori.Apple-Server/routes/feedbacks/feedbacks.js
@@ -28,23 +28,35 @@ async function createFeedback(req, res, next) {
     }
 }
 
-// request data: team_id, reflection_id
-// query string: type
-// reponse data: id, type, keyword, content, start_content, from_id, to_id, team_id, reflection_id
+// request data: team_id, reflection_id, type
+// response data: id, type, keyword, content, start_content, from_id, to_id, team_id, reflection_id
 // 특정 type을 만족하는 feedback을 불러온다.
+// 만약 reflection_id 가 recent인 경우에는 가장 최근 회고에서 feedback을 불러온다.
 const getCertainTypeFeedbackAll = async (req, res) => {
     try {
         const { type } = req.query;
         const { team_id, reflection_id } = req.params;
 
+        if (reflection_id == "recent") {
+            const teamData = await team.findByPk(team_id)
+            const recentReflectionId = teamData.recent_reflection_id;
+            const feedbackData = await feedback.findAll({
+                where: {
+                    team_id: team_id,
+                    reflection_id: recentReflectionId,
+                    type: type
+                }
+            })  
+            return res.status(200).json({'success':true, 'message':'최근 회고 피드백 조회 성공','detail':feedbackData});         
+        }
         const feedbackData = await feedback.findAll({
             where: {
-                type: type,
                 team_id: team_id,
-                reflection_id: reflection_id
+                reflection_id: reflection_id,
+                type: type
             }
         })
-        return res.status(200).json({'success':true, 'message':'조회 성공','detail':feedbackData});
+        return res.status(200).json({'success':true, 'message':'피드백 조회 성공','detail':feedbackData});
     } catch (error) {
         console.log(error);
         return res.status(400).json({"success":false, "message":"조회 실패"})
@@ -53,5 +65,5 @@ const getCertainTypeFeedbackAll = async (req, res) => {
 
 module.exports = {
     createFeedback,
-    getCertainTypeFeedbackAll
+    getCertainTypeFeedbackAll,
 };
--- Maddori.Apple-Server/routes/feedbacks/index.js
+++ Maddori.Apple-Server/routes/feedbacks/index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const router = new express.Router({ mergeParams: true });
 const {
     createFeedback,
-    getCertainTypeFeedbackAll
+    getCertainTypeFeedbackAll,
 } = require('./feedbacks');
 
 router.post('/', createFeedback);
 router.get('/', getCertainTypeFeedbackAll);
-
-//* 특정 타입의 피드백 모두 가져오기
-//router.get("/:team_id/reflection/recent/feedbacks", getCertainTypeRecentFeedbackAll);
 module.exports = router;
