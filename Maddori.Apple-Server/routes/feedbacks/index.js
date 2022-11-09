52eaea2e770946579199e7ad5b8f6ab1e2d587d9
unanchoi <unusualc@likelion.org>
2022-11-09 10:00:59 +0900

    [FEAT] 피드백 수정 삭제 API route 추가

--- Maddori.Apple-Server/routes/feedbacks/index.js
+++ Maddori.Apple-Server/routes/feedbacks/index.js
@@ -3,8 +3,12 @@ const router = new express.Router({ mergeParams: true });
 const {
     createFeedback,
     getCertainTypeFeedbackAll,
+    updateFeedback,
+    deleteFeedback
 } = require('./feedbacks');
 
 router.post('/', createFeedback);
 router.get('/', getCertainTypeFeedbackAll);
+router.put('/:feedback_id', updateFeedback);
+router.delete("/:feedback_id", deleteFeedback);
 module.exports = router;
