e603da2d5e26a3a4b0662e4fc81220d5c3f9e4fa
hwiwonK <gnldnjs2469@gmail.com>
2022-11-15 00:11:36 +0900

    [CHORE] URI slash 추가, getTeamAndUserFeedback에 미들웨어 적용(#22)

--- Maddori.Apple-Server/routes/feedbacks/index.js
+++ Maddori.Apple-Server/routes/feedbacks/index.js
@@ -21,6 +21,6 @@ router.get('/', [userTeamCheck], getCertainTypeFeedbackAll);
 router.put('/:feedback_id', [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('Before')], updateFeedback);
 router.delete("/:feedback_id", [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('Before')], deleteFeedback);
 router.get('/from-me', [userTeamCheck, reflectionTimeCheck], getFromMeToCertainMemberFeedbackAll);
-router.get("/from-team", getTeamAndUserFeedback); 
+router.get("/from-team", [userTeamCheck, reflectionStateCheck('Progressing')], getTeamAndUserFeedback); 
 
 module.exports = router;
