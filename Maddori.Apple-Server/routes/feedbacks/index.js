6b9abcb74110fd44f936563593d8e0c2f785937d
unanchoi <unusualc@likelion.org>
2022-11-21 23:19:39 +0900

    [CHORE] 미들웨어에 조건 추가

--- Maddori.Apple-Server/routes/feedbacks/index.js
+++ Maddori.Apple-Server/routes/feedbacks/index.js
@@ -20,7 +20,7 @@ router.post('/', [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('Befo
 router.get('/', [userTeamCheck, reflectionStateCheck], getCertainTypeFeedbackAll);
 router.put('/:feedback_id', [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('Before')], updateFeedback);
 router.delete("/:feedback_id", [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('Before')], deleteFeedback);
-router.get('/from-me', [userTeamCheck, reflectionTimeCheck], getFromMeToCertainMemberFeedbackAll);
+router.get('/from-me', [userTeamCheck, reflectionTimeCheck('Done')], getFromMeToCertainMemberFeedbackAll);
 router.get("/from-team", [userTeamCheck, reflectionStateCheck('Progressing')], getTeamAndUserFeedback); 
 
 module.exports = router;
