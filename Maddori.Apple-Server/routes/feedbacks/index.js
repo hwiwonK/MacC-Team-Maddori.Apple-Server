bc79d1f18ce7c5e4ee1e6f5c56e94da17c6378a2
hwiwonK <gnldnjs2469@gmail.com>
2022-11-23 01:49:53 +0900

    [FIX] 회고 상태 검증 미들웨어 오류 해결 및 미들웨어 적용 사항 수정(#70)

--- Maddori.Apple-Server/routes/feedbacks/index.js
+++ Maddori.Apple-Server/routes/feedbacks/index.js
@@ -15,11 +15,11 @@ const {
     reflectionStateCheck
 } = require('../../middlewares/auth');
 
-router.post('/', [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('Before')], createFeedback);
+router.post('/', [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('SettingRequired', 'Before')], createFeedback);
 router.get('/', [userTeamCheck, reflectionStateCheck('Done')], getCertainTypeFeedbackAll);
-router.put('/:feedback_id', [userTeamCheck, reflectionStateCheck('Before'), reflectionTimeCheck ], updateFeedback);
-router.delete("/:feedback_id", [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('Before')], deleteFeedback);
+router.put('/:feedback_id', [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('SettingRequired', 'Before')], updateFeedback);
+router.delete("/:feedback_id", [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('SettingRequired', 'Before')], deleteFeedback);
 router.get('/from-me', [userTeamCheck, reflectionTimeCheck], getFromMeToCertainMemberFeedbackAll);
-router.get("/from-team", [userTeamCheck, reflectionStateCheck('Progressing')], getTeamAndUserFeedback); 
+router.get("/from-team", [userTeamCheck, reflectionTimeCheck, reflectionStateCheck('Progressing')], getTeamAndUserFeedback); 
 
 module.exports = router;
