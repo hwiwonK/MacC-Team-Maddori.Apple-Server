7ac033b28cc4b477190b3024f97b22b807c9780b
unanchoi <unusualc@likelion.org>
2022-11-23 16:24:53 +0900

    [CHORE] 피드백 입력 제한 조건  수정

--- Maddori.Apple-Server/middlewares/validate.js
+++ Maddori.Apple-Server/middlewares/validate.js
@@ -11,24 +11,13 @@ const validateFeedback = async (req, res, next) => {
             throw Error('content 200자 초과')
         }
         
-        const numberPattern = /[0-9]/;
-        const alphaPattern = /[a-zA-Z]/;
-        const koreanPattern = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
-        const symbolPattern = /[~!@#\#$%<>^&*]/;
+        if ( keyword. length > 11 ) {
 
-        if (alphaPattern.test(keyword) && koreanPattern.test(keyword)) {
-            throw Error('한글, 알파벳을 혼용');
         }
 
         if (alphaPattern.test(keyword)) {
             if (keyword.length > 15) {
                 throw Error('키워드 15글자 초과');
-            }
-        }
-
-        if (koreanPattern.test(keyword)) {
-            if (keyword.length > 11) {
-                throw Error('키워드 11글자 초과');
             }
         }
 
@@ -41,16 +30,6 @@ const validateFeedback = async (req, res, next) => {
         })
     }
 }
-
-
-// {
-//     "type" : "Continue",
-//     "keyword" : "화이팅",
-//     "content": "너무 잘해요",
-//     "start_content": "앞으로도 화이팅",
-//     "to_id": 2
-// }
-
 
 module.exports = {
     validateFeedback
