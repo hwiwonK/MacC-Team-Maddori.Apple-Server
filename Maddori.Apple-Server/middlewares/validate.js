4320802f17a10abf237d78fafa8abb55186c930b
hwiwonK <gnldnjs2469@gmail.com>
2022-11-24 18:49:25 +0900

    [CHORE] 피드백 입력값 검증 express-validator 적용(#72)

--- Maddori.Apple-Server/middlewares/validate.js
+++ Maddori.Apple-Server/middlewares/validate.js
@@ -1,55 +1,61 @@
 const { body, validationResult } = require('express-validator');
 
-const validateFeedback = async (req, res, next) => {
+const validateFeedback = [
+    body('keyword')
+        .not().isEmpty()
+        .withMessage('keyword 값이 비어있음')
+        .isString()
+        .withMessage('keyword는 문자열 형식이어야 함')
+        .isLength({ max: 11 })
+        .withMessage('keyword 글자 수 제한(11글자) 초과'),
+    body('content')
+        .not().isEmpty()
+        .withMessage('content 값이 비어있음')
+        .isString()
+        .withMessage('content는 문자열 형식이어야 함')
+        .isLength({ max: 200 })
+        .withMessage('keyword 글자 수 제한(200글자) 초과'),
+    body('start_content')
+        .isString()
+        .withMessage('start_content는 문자열 형식이어야 함')
+        .isLength({ max: 200 })
+        .withMessage('start_content 글자 수 제한(200글자) 초과'),
     
-    try {
-        const { type, keyword, content, start_content, to_id } = req.body
-
-        if ( start_content.length > 200 ) {
-            throw Error('start 200자 초과');
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                message: '입력 값의 형식이 잘못됨',
+                detail: errors.array()[0].msg
+            })
         }
-
-        if ( content.length > 200 ) {
-            throw Error('content 200자 초과')
-        }
-        
-        if ( keyword. length > 11 ) {
-            throw Error('키워드 11글자 초과')
-        }
-
         next();
-        
-    } catch (error) {
-        return res.status(400).json({
-            success: false,
-            message: '입력 값의 형식이 잘못됨',
-            detail: error.message
-        })
     }
-}
+]
 
 // username 형식 검증 (최대 6글자, 영어/한국어 가능, 특수문자 불가능, 띄어쓰기 불가능)
 const validateUsername = [
     body('username')
-    .not().isEmpty()
-    .withMessage('username 값이 비어있음')
-    .isString()
-    .isLength({ max: 6 })
-    .withMessage('username 글자 수 제한(6글자) 초과')
-    .custom((value) => {
-        const pattern = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/ // 특수문자
-        const pattern2 = /\p{Extended_Pictographic}/u; // 이모지
-        if (!!pattern.test(value) || !!pattern2.test(value)) {
-            throw new Error('username 특수문자 포함 불가');
-        }
-        return true;
-    })
-    .custom((value) => !/\s/.test(value))
-    .withMessage('username 띄어쓰기 포함 불가'),
+        .not().isEmpty()
+        .withMessage('username 값이 비어있음')
+        .isString()
+        .withMessage('username은 문자열 형식이어야 함')
+        .isLength({ max: 6 })
+        .withMessage('username 글자 수 제한(6글자) 초과')
+        .custom((value) => {
+            const pattern = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/ // 특수문자
+            const pattern2 = /\p{Extended_Pictographic}/u; // 이모지
+            if (!!pattern.test(value) || !!pattern2.test(value)) {
+                throw new Error('username 특수문자 포함 불가');
+            }
+            return true;
+        })
+        .custom((value) => !/\s/.test(value))
+        .withMessage('username 띄어쓰기 포함 불가'),
 
     (req, res, next) => {
         const errors = validationResult(req);
-        console.log(errors);
         if (!errors.isEmpty()) {
             return res.status(400).json({
                 success: false,
