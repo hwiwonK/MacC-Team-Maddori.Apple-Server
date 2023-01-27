2e27e9065c731e6044e6c86952327f506154f164
hwiwonK <gnldnjs2469@gmail.com>
2023-01-27 23:45:12 +0900

    [ADD] getCertainTeamName version 2 URI 추가(#117)

--- Maddori.Apple-Server/routes/v2/teams/index.js
+++ Maddori.Apple-Server/routes/v2/teams/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = new express.Router({ mergeParams: true });
 
+// version 1 team api (version 1 api 이용 유지)
+const {
+    getCertainTeamName
+} = require('../../v1/teams/teams');
+
+// version 2 team api
 const {
     createTeam,
     getCertainTeamDetail
 } = require('./teams');
+
+// middlewares
 const {
     userCheck,
     userTeamCheck,
@@ -17,6 +25,7 @@ const {
 // user auth 검증
 router.use('/', userCheck);
 // handler
+router.get('/', getCertainTeamName);
 router.post('/', [validateTeamname], createTeam);
 router.get('/:team_id', [userTeamCheck], getCertainTeamDetail);
 
